934dddaccb79b3cfff35484d2091d985803c6e6d
padi-dev-vietnt2 <vietnt2@paditech.com>
2022-05-27 16:51:18 +0700

    first commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,32 @@
+pragma solidity >= 0.4.0 < 0.9.0;
+
+import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
+    string private _greeting = "Hello, World!";
+    address private _owner;
+
+    // constructor() public {
+    //     _owner = msg.sender;
+    // }
+
+    // modifier onlyOwner() {
+    //     require(
+    //         msg.sender == _owner,
+    //         "Ownable: caller is not the owner"
+    //     );
+    //     _;
+    // }
+
+    function greet() external view returns(string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner{
+        _greeting = greeting;
+    }
+
+    // function owner() public view returns(address) {
+    //     return _owner;
+    // }
+}
